875a4d01a780a5998e81626e23d3f2a7dbdb6065
skylineharsh <skylineharsh@gmail.com>
2022-11-05 16:21:08 +0530

    code fix for prime

--- prime.js
+++ prime.js
@@ -1,3 +1,24 @@
+
 function checkprime(num){
-for(let i=1;i<=num;i++){
-if(num%i==0){
+
+	let factor=0;
+
+	for(let i=1; i<=num; i++){
+
+		if(num%i==0){
+				
+			factor++;
+		}
+	}
+
+	if(factor==2){
+
+		console.log(num, ": is prime");
+
+	}else{
+
+		console.log(num, ": is not prime);
+
+	}
+
+checkprime(15);
